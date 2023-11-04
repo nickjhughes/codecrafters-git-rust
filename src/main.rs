a4091ee228e53ae43b7b247941b6461c61b3fefe
Nick Hughes <689543+nickjhughes@users.noreply.github.com>
2023-11-04 14:35:44 +1100

    Support init

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,20 @@
-#[allow(unused_imports)]
+use anyhow::Result;
 use std::env;
-#[allow(unused_imports)]
 use std::fs;
 
-fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage
-    // let args: Vec<String> = env::args().collect();
-    // if args[1] == "init" {
-    //     fs::create_dir(".git").unwrap();
-    //     fs::create_dir(".git/objects").unwrap();
-    //     fs::create_dir(".git/refs").unwrap();
-    //     fs::write(".git/HEAD", "ref: refs/heads/master\n").unwrap();
-    //     println!("Initialized git directory")
-    // } else {
-    //     println!("unknown command: {}", args[1])
-    // }
+fn main() -> Result<()> {
+    let args: Vec<String> = env::args().collect();
+    match args[1].as_str() {
+        "init" => {
+            fs::create_dir(".git")?;
+            fs::create_dir(".git/objects")?;
+            fs::create_dir(".git/refs")?;
+            fs::write(".git/HEAD", "ref: refs/heads/master\n")?;
+            println!("Initialized git directory")
+        }
+        cmd => {
+            anyhow::bail!("unknown command: {cmd}")
+        }
+    }
+    Ok(())
 }
